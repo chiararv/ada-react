ebdc4fcc6a5deb93407a4493e3de93443e8ccd06
Chiara Rosa Velazco <chiara.rosavelazco@gmail.com>
2020-07-23 09:38:10 -0300

    Updates

---
+++ precache-manifest.0451af2200a96577673b1e1f562c6939.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f903530f8504d3463e78a341b23af9f1",
+    "url": "/ada-react/index.html"
+  },
+  {
+    "revision": "36716635df2cc58a13e8",
+    "url": "/ada-react/static/css/main.c69b1962.chunk.css"
+  },
+  {
+    "revision": "0c71868be90f9e793ff8",
+    "url": "/ada-react/static/js/2.b5f6da6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ada-react/static/js/2.b5f6da6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36716635df2cc58a13e8",
+    "url": "/ada-react/static/js/main.3a53bd9f.chunk.js"
+  },
+  {
+    "revision": "dadd2ee8bd759a653719",
+    "url": "/ada-react/static/js/runtime-main.680ee914.js"
+  },
+  {
+    "revision": "8af3ced1f6cb605890f0826aec5f2f7e",
+    "url": "/ada-react/static/media/01.8af3ced1.jpg"
+  },
+  {
+    "revision": "4d39d9d821fb791b66f1e8959563de75",
+    "url": "/ada-react/static/media/02.4d39d9d8.jpg"
+  },
+  {
+    "revision": "0f13be35fa39528916cd66eb27e8d58a",
+    "url": "/ada-react/static/media/03.0f13be35.jpg"
+  },
+  {
+    "revision": "6527d1c1c6275d4c801fa36e7f6cc68f",
+    "url": "/ada-react/static/media/04.6527d1c1.jpg"
+  },
+  {
+    "revision": "383ca1f9f298074ce26180acba0fff4e",
+    "url": "/ada-react/static/media/05.383ca1f9.jpg"
+  },
+  {
+    "revision": "b32e16acf7727fd9d32f958d4ca26224",
+    "url": "/ada-react/static/media/06.b32e16ac.jpg"
+  },
+  {
+    "revision": "12325b1994250e6c6d127034bdb79d68",
+    "url": "/ada-react/static/media/07.12325b19.jpg"
+  },
+  {
+    "revision": "d22712c77383877dad1881fd46875de7",
+    "url": "/ada-react/static/media/08.d22712c7.jpg"
+  },
+  {
+    "revision": "bd78f89705c7183bcb785532ad656032",
+    "url": "/ada-react/static/media/09.bd78f897.jpg"
+  }
+]);
